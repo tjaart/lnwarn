838525c2bb7c15c6cbb2a40bbc516c423012a6f5
Tjaart <tjaartblig@gmail.com>
2023-06-25 20:10:26 +0200

    more cleanup

--- LnWarn.Cmd/Program.cs
+++ LnWarn.Cmd/Program.cs
@@ -52,40 +52,45 @@ class Program
         PatternMatchingResult matchingFiles = matcher.Execute(
             directoryInfoWrapper);
 
+        Console.WriteLine($"Analyzing directory \"{directoryInfoWrapper.FullName}\" ...");
+        
         var lineCounter = new LineCounter(filters);
 
         var lineCountResults = new List<LineCountResult>();
-            
+
         foreach (var matchingFilesFile in matchingFiles.Files)
         {
             await using var stream = File.OpenRead(matchingFilesFile.Path);
             var lineCount = await lineCounter.CountLines(stream);
-            lineCountResults.Add(new LineCountResult(matchingFilesFile.Path, matchingFilesFile.Stem, lineCount));
+            lineCountResults.Add(new LineCountResult(matchingFilesFile.Path, matchingFilesFile.Stem ?? "??", lineCount));
         }
-
-        Console.WriteLine($"Analyzing directory {directoryInfoWrapper.FullName}");
             
         var ruleBreakers = lineCountResults.Where(c => c.LineCount > MaxLines).ToList();
         if (ruleBreakers.Any())
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            await Console.Error.WriteLineAsync();
-            DrawHorizontalLine();
-            await Console.Error.WriteLineAsync($"Count of lines with minimum length of {MinLineLength} exceeded the maximum number of allowed lines {MaxLines} per file in the following {ruleBreakers.Count} file{(ruleBreakers.Count > 1 ? "s" : "")}");
-            DrawHorizontalLine();
-            await Console.Error.WriteLineAsync($"{"Path",-50}{"Lines",-10}");
-            foreach (var (_, stem, lineCount) in ruleBreakers)
-            {
-                await Console.Error.WriteLineAsync($"{stem,-50}{lineCount,-10}");
-            }
-
-            DrawHorizontalLine();
-            Console.ResetColor();
-
+            await WriteFailureResult(ruleBreakers);
             return 1;
         }
             
         return 0;
+    }
+
+    private async Task WriteFailureResult(List<LineCountResult> ruleBreakers)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        await Console.Error.WriteLineAsync();
+        DrawHorizontalLine();
+        await Console.Error.WriteLineAsync(
+            $"Count of lines with minimum length of {MinLineLength} exceeded the maximum number of allowed lines {MaxLines} per file in the following {ruleBreakers.Count} file{(ruleBreakers.Count > 1 ? "s" : "")}");
+        DrawHorizontalLine();
+        await Console.Error.WriteLineAsync($"{"Path",-50}{"Lines",-10}");
+        foreach (var (_, stem, lineCount) in ruleBreakers)
+        {
+            await Console.Error.WriteLineAsync($"{stem,-50}{lineCount,-10}");
+        }
+
+        DrawHorizontalLine();
+        Console.ResetColor();
     }
 
     private void DrawHorizontalLine() => Console.Error.WriteLineAsync(new string('─', 150));
